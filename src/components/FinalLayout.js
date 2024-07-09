aae11bf88fd205a15ced9baad0fbd351b68fe49b
Moonish Maredia <moonishmaredia@Moonishs-MBP.attlocal.net>
2024-07-09 03:49:41 -0500

    Added more stuff

--- src/components/FinalLayout.js
+++ src/components/FinalLayout.js
@@ -17,99 +17,29 @@ import {
 export default function FinalLayout() { 
 
     const [showOptions, setShowOptions] = useState(true)
-    const [fromDate, setFromDate] = useState(new Date());
-    const [toDate, setToDate] = useState(new Date());
-    const [selectedSeries, setSelectedSeries] = useState([]);
-    const[metric, setMetric] = useState(null)  
-    const[chartType, setChartType] = useState(null)
-    const minDate = new Date(1999, 1, 1);
-    const maxDate = new Date(2024, 5, 1);
-    const [layoutScenario, setLayoutScenario] = useState("landing-view")
-
-    useEffect(()=>{
-        if(!showOptions) {
-            setLayoutScenario("no-options-view")
-        } else {
-            if(chartType) {
-                if(chartType.value==="time-series") {
-                    setLayoutScenario("series-view")
-                } else if(chartType.value==="compare") {
-                    setLayoutScenario("compare-view")
-                }
-            } else {
-                setLayoutScenario("landing-view")
-            }
-        }
-    }, [showOptions, chartType])
 
 return (
     <div className="main">
         <Header
             showOptions={showOptions}
-            setShowOptions={setShowOptions}
-        />
-        <div className="selection-view">
-            <SelectionMenu
-            />
-            <div className="item1">
-                <MyPlotlyChart />
+            setShowOptions={setShowOptions}/>
+        {showOptions && 
+            <div className="selection-view">
+                <SelectionMenu
+                />
+                <div className="item1">
+                    <MyPlotlyChart />
+                </div>
             </div>
-
-        </div>
-
-        {/* {layoutScenario==="landing-view" &&
-            <LandingView
-                chartTypeOptions={chartTypeOptions}
-                chartTypeStateVar={chartType}
-                setChartTypeStateVar={setChartType}/>
+        }
+        {!showOptions && 
+            <div className="no-options-view">
+                <div className="item1">
+                    <MyPlotlyChart />
+                </div>
+            </div>
         }
 
-        {layoutScenario==="series-view" &&
-            <SeriesView 
-                metricOptionsArr={metricOptions}
-                metricsVar={metric}
-                setMetricStateVar={setMetric}
-                fromDateVar={fromDate}
-                setFromDateVar={setFromDate}
-                minDate={minDate}
-                maxDate={maxDate}
-                toDateVar={toDate}
-                setToDateVar={setToDate}
-                selectedSeries={selectedSeries}
-                handleCheck={handleCheck}
-                chartType={chartType}
-                chartTypeOptions={chartTypeOptions}
-                setChartTypeStateVar={setChartType}/>
-        }
 
-        {layoutScenario==="compare-view" &&
-            <CompareView 
-                setFromDateVar={setFromDate}
-                minDate={minDate}
-                maxDate={maxDate}
-                setToDateVar={setToDate}
-                selectedSeries={selectedSeries}
-                handleCheck={handleCheck}
-                chartTypeOptions={chartTypeOptions}
-                setChartTypeStateVar={setChartType}/>
-        }
-
-        {layoutScenario==="no-options-view" && 
-            <NoOptionsView
-            />
-        } */}
-
-        {/* <div className="main-options">
-            <ToggleSelector 
-                isCompare={isCompare}
-                toggleMenu={toggleMenu}
-                toggleText1={"Time Series"}
-                toggleText2={"Compare"}/>
-            <OptionsMenu 
-                isCompare={isCompare}/>
-        </div>
-        <div className="item1">
-            <MyPlotlyChart />
-        </div> */}
     </div>
     )}

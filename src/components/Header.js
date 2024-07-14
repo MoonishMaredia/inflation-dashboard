850d1ed8d0da66ae97df48f5de84da24f3ce6ff2
Moonish Maredia <moonishmaredia@Moonishs-MBP.attlocal.net>
2024-07-13 21:08:41 -0500

    Keeping helpful API call button for future debug of backend. Will remove in next commit for production deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import flagLogo from '../assets/flag.svg'
+import { updateData } from '../utils/api'
 
 export default function Header({showOptions, setShowOptions}) {
 
+    async function makeAPICall() {
+        const inputRequest = {
+            addDate: "2024-06-01",
+            // weightTableUpdated: false
+        }
+        const result = await updateData(inputRequest)
+        console.log(result)
+    }
 
     return (
         <div className="header-layout">
             <div className="header">
                 <img className="header-logo" src={flagLogo}></img>
                 <p>US CPI Inflation Dashboard</p>
+                <button onClick={makeAPICall}> Test For Data Update</button>
             </div>
         </div>
     )
--- src/utils/api.js
+++ src/utils/api.js
@@ -36,4 +36,17 @@ export async function getMaxDate() {
         console.error("Error making API call:", error);
     }
 }
+
+export async function updateData(inputObject) {
+
+    console.log(inputObject)
+
+    try {
+        const data = await axios.post(process.env.REACT_APP_INFLATION_URL + "/api/v1/updateDB", inputObject)
+        .then(res => res.data);
+        return data
+    } catch (error) {
+        console.error("Error making API call:", error);
+    }
+}
       
